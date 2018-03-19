1aae9d6a2eb517f478c0d94b8e77e90e6e80b35f
Engineer Loft <engineerloft@gmail.com>
2018-03-19 14:15:10 +0100

    Add TS FIFO support.
    Signed-off-by: Engineer Loft <engineerloft@gmail.com>

--- kernel/module_netlink.c
+++ kernel/module_netlink.c
@@ -36,6 +36,19 @@ DEFINE_TIMER(mytimer, mytimer_handler, 0, 0);
 struct sock *nl_sk = NULL;
 struct timespec ts_current;
 
+struct myts {
+		u64 sec;
+		u64 nsec;
+		int valid;
+};
+
+struct myqueue_element {
+		struct list_head next;
+		struct myts ts;
+};
+
+static struct list_head queue;
+
 static void myprintk_ts(struct timespec *ts)
 {
 	printk("%.2lu:%.2lu:%.2lu:%.6lu \n",
@@ -47,7 +60,19 @@ static void myprintk_ts(struct timespec *ts)
 
 static void mytimer_handler(unsigned long data)
 {
+		struct myqueue_element *new_ts;
+
 		getnstimeofday(&ts_current);
+
+		new_ts = kmalloc(sizeof(struct myqueue_element),
+			GFP_ATOMIC);
+
+		(new_ts->ts).sec = ts_current.tv_sec;
+		(new_ts->ts).nsec = ts_current.tv_nsec;
+		(new_ts->ts).valid = 1;
+
+		list_add_tail(&new_ts->next, &queue);
+
 		mod_timer(&mytimer, jiffies + TIMER_PERIOD);
 }
 
@@ -60,9 +85,10 @@ static void netlink_f_recv_msg(struct sk_buff *skb)
 	int msg_size;
 	char msg[MSG_SIZE_MAX];
 	int res;
-	struct timespec *ts = &(ts_current);
+	//struct timespec *ts = &(ts_current);
 	char *recv_buf = NULL;
 	char *error_string = "ERROR";
+	char *queue_empty = "QUEUE EMPTY";
 
 	msg_size = strlen(msg);
 	nlh = (struct nlmsghdr*)skb->data;
@@ -70,14 +96,26 @@ static void netlink_f_recv_msg(struct sk_buff *skb)
 	
 	recv_buf = (char *) nlmsg_data(nlh);
 	
-	if(!strcmp("GETTS",recv_buf)) {
-		sprintf(msg,"%.2lu:%.2lu:%.2lu:%.6lu",
-			(ts->tv_sec / 3600) % (24),
-			(ts->tv_sec / 60) % (60),
-			ts->tv_sec % 60,
-			ts->tv_nsec / 1000);
+	if (!list_empty(&queue)) {
+		if(!strcmp("GETTS",recv_buf)) {
+			struct myqueue_element *qe;
+			qe = list_first_entry(&queue, struct myqueue_element,
+			next);
+			list_del(&qe->next);
+			
+			sprintf(msg,"%.2llu:%.2llu:%.2llu:%.6llu:v%d",
+				(qe->ts.sec / 3600) % (24),
+				(qe->ts.sec / 60) % (60),
+				qe->ts.sec % 60,
+				qe->ts.nsec / 1000,
+				qe->ts.valid);
+			
+			kfree(qe);
+		} else {
+			strcpy(msg,error_string);
+		}
 	} else {
-		strcpy(msg,error_string);
+		strcpy(msg,queue_empty);
 	}
 
 	/* Send the msg from kernel to the user */
@@ -118,6 +156,8 @@ static int __init netlink_module_init(void)
 
 	}
 	
+	INIT_LIST_HEAD(&queue);
+	
 	mod_timer(&mytimer, jiffies+TIMER_PERIOD);
 
 	return 0;
@@ -125,11 +165,20 @@ static int __init netlink_module_init(void)
 
 static void __exit netlink_module_exit(void) 
 {
+	struct myqueue_element *qe;
+	
 	getnstimeofday(&ts_current);
 	pr_info("%s: Exiting... \n",__func__);
 	myprintk_ts(&ts_current);
 	del_timer(&mytimer);
 	netlink_kernel_release(nl_sk);
+	
+	while (!list_empty(&queue)) {
+		qe = list_first_entry(&queue, struct myqueue_element,
+			next);
+		list_del(&qe->next);
+		kfree(qe);
+	}
 }
 
 module_init(netlink_module_init); 
