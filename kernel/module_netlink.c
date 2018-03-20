ac8c29d5627bd299ef4befdb5d497d980900cea6
Engineer Loft <engineerloft@gmail.com>
2018-03-20 11:11:27 +0100

    Create a new header file for the custom queue functions and add lock mechanisms.
    Signed-off-by: Engineer Loft <engineerloft@gmail.com>

--- kernel/module_netlink.c
+++ kernel/module_netlink.c
@@ -26,6 +26,8 @@
 #include <net/sock.h>
 #include <linux/netlink.h>
 #include <linux/skbuff.h>
+
+#include "myqueue.h"
   
 #define NETLINK_CUSTOM_PROTO 31
 
@@ -41,21 +43,8 @@ DEFINE_TIMER(mytimer, mytimer_handler, 0, 0);
 struct sock *nl_sk = NULL;
 struct timespec ts_current;
 
-struct myts {
-		u64 sec;
-		u64 nsec;
-		u64 seq;
-		int valid;
-		int tx_rx;
-};
-
-struct myqueue_element {
-		struct list_head next;
-		struct myts ts;
-};
-
-static struct list_head rx_queue;
-static struct list_head tx_queue;
+static struct myqueue rx_queue;
+static struct myqueue tx_queue;
 
 static void myprintk_ts(struct timespec *ts)
 {
@@ -64,67 +53,6 @@ static void myprintk_ts(struct timespec *ts)
 		(ts->tv_sec / 60) % (60),
 		ts->tv_sec % 60,
 		ts->tv_nsec / 1000);
-}
-
-static struct myqueue_element * kmalloc_qe(struct timespec *ts, 
-	int valid, int tx_rx, u64 seq)
-{
-	struct myqueue_element *qe = NULL;
-	
-	qe = kmalloc(sizeof(struct myqueue_element),
-			GFP_ATOMIC);
-	if(!qe)
-		return NULL;
-			
-	(qe->ts).sec = ts->tv_sec;
-	(qe->ts).nsec = ts->tv_nsec;
-	(qe->ts).valid = valid;
-	(qe->ts).tx_rx = tx_rx;
-	(qe->ts).seq = seq;
-	
-	return qe;
-}
-
-static int enqueue(struct list_head *q, struct myqueue_element *qe)
-{	
-	list_add_tail(&qe->next, q);
-	
-	return 0;
-}
-
-static struct myqueue_element *dequeue(struct list_head *q)
-{
-	struct myqueue_element *qe = NULL;
-	
-	qe = list_first_entry(q, struct myqueue_element,
-		next);
-	list_del(&qe->next);
-	
-	return qe;
-}
-
-static void kfree_queue(struct list_head *q)
-{
-	struct myqueue_element *qe = NULL;
-	
-	while (!list_empty(q)) {
-		qe = list_first_entry(q, struct myqueue_element,
-			next);
-		list_del(&qe->next);
-		kfree(qe);
-	}
-}
-
-static void sprintf_ts(char *buf, struct myqueue_element *qe)
-{
-	sprintf(buf,"%.2llu:%.2llu:%.2llu:%llu:v%d:t%d:s%llu",
-				(qe->ts.sec / 3600) % (24),
-				(qe->ts.sec / 60) % (60),
-				qe->ts.sec % 60,
-				qe->ts.nsec / 1000,
-				qe->ts.valid,
-				qe->ts.tx_rx,
-				qe->ts.seq);
 }
 
 static void mytimer_handler(unsigned long data)
@@ -150,7 +78,7 @@ static void mytimer_handler(unsigned long data)
 		mod_timer(&mytimer, jiffies + TIMER_PERIOD);
 }
 
-#define MSG_SIZE_MAX 256
+#define MSG_SIZE_MAX 512
 static void netlink_f_recv_msg(struct sk_buff *skb)
 {
 	struct nlmsghdr *nlh;
@@ -181,7 +109,7 @@ static void netlink_f_recv_msg(struct sk_buff *skb)
 			struct myqueue_element *qe = NULL;
 			
 			if(rx_queue_cmd) {
-				if (!list_empty(&rx_queue)) {
+				if (!is_queue_empty(&rx_queue)) {
 					qe = dequeue(&rx_queue);
 					sprintf_ts(msg,qe);
 				} else {
@@ -189,7 +117,7 @@ static void netlink_f_recv_msg(struct sk_buff *skb)
 				}
 			}
 			else {
-				if (!list_empty(&tx_queue)) {
+				if (!is_queue_empty(&tx_queue)) {
 					qe = dequeue(&tx_queue);
 					sprintf_ts(msg,qe);
 				} else {
@@ -240,8 +168,8 @@ static int __init netlink_module_init(void)
 
 	}
 	
-	INIT_LIST_HEAD(&rx_queue);
-	INIT_LIST_HEAD(&tx_queue);
+	queue_init(&rx_queue);
+	queue_init(&tx_queue);
 	
 	mod_timer(&mytimer, jiffies+TIMER_PERIOD);
 
