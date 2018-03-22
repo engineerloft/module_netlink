ee7e23e20741d3e5b18d6c0a3c403800950cd071
Engineer Loft <engineerloft@gmail.com>
2018-03-22 19:32:42 +0100

    Upgrade the example design using the timestamps.
    Signed-off-by: Engineer Loft <engineerloft@gmail.com>

--- kernel/module_netlink.c
+++ kernel/module_netlink.c
@@ -2,9 +2,63 @@
 #include <net/genetlink.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/interrupt.h>
+#include <linux/timer.h>
+#include <linux/time.h>
+#include <linux/jiffies.h>
+#include <net/sock.h>
+#include <linux/skbuff.h>
+
+#include "myqueue.h"
 
 //Code based on 
 //http://people.ee.ethz.ch/~arkeller/linux/multi/kernel_user_space_howto-3.html
+
+#define TIMER_PERIOD 1*HZ
+#define TX_TS_PERIOD 5
+#define RX_TS_PERIOD 5
+static int mytimer_events = 0;
+static u64 tx_seq = 0;
+static u64 rx_seq = 0;
+static void mytimer_handler(unsigned long data);
+DEFINE_TIMER(mytimer, mytimer_handler, 0, 0);
+struct timespec ts_current;
+
+static struct myqueue rx_queue;
+static struct myqueue tx_queue;
+
+static void myprintk_ts(struct timespec *ts)
+{
+	printk("%.2lu:%.2lu:%.2lu:%.6lu \n",
+		(ts->tv_sec / 3600) % (24),
+		(ts->tv_sec / 60) % (60),
+		ts->tv_sec % 60,
+		ts->tv_nsec / 1000);
+}
+
+static void mytimer_handler(unsigned long data)
+{
+		struct myqueue_element *qe = NULL;
+		
+		getnstimeofday(&ts_current);
+		//myprintk_ts(&ts_current);
+
+		if (mytimer_events % TX_TS_PERIOD == 0) {
+			qe = kmalloc_qe(&ts_current, 1, 0, tx_seq);
+			enqueue(&tx_queue, qe);
+			tx_seq++;
+		}
+			
+		if (mytimer_events % RX_TS_PERIOD == 0) {
+			qe = kmalloc_qe(&ts_current, 1, 1, rx_seq);
+			enqueue(&rx_queue, qe);
+			rx_seq++;
+		}
+		
+		mytimer_events++;
+
+		mod_timer(&mytimer, jiffies + TIMER_PERIOD);
+}
 
 /* attributes (variables):
  * the index in this enum is used as a reference for the type,
@@ -30,7 +84,7 @@ static struct nla_policy doc_exmpl_genl_policy[DOC_EXMPL_A_MAX + 1] = {
 static struct genl_family doc_exmpl_gnl_family = {
 	//.id = GENL_ID_GENERATE,         //Genetlink should generate an id
 	.hdrsize = 0,
-	.name = "CONTROL_EXMPL",        //The name of this family, used by userspace application
+	.name = "HA_TS_FAMILY",        //The name of this family, used by userspace application
 	.version = VERSION_NR,          //Version number  
 	.maxattr = DOC_EXMPL_A_MAX,
 };
@@ -40,18 +94,27 @@ static struct genl_family doc_exmpl_gnl_family = {
  */
 enum {
 	DOC_EXMPL_C_UNSPEC,
-	DOC_EXMPL_C_ECHO,
+	DOC_EXMPL_C_GETTS,
 	__DOC_EXMPL_C_MAX,
 };
 #define DOC_EXMPL_C_MAX (__DOC_EXMPL_C_MAX - 1)
 
-//An echo command, receives a message, prints it and sends another message back
-int doc_exmpl_echo(struct sk_buff *skb_2, struct genl_info *info) {
+#define MSG_SIZE_MAX 512
+int doc_exmpl_getts(struct sk_buff *skb_2, struct genl_info *info) {
 	struct nlattr *na;
 	struct sk_buff *skb;
 	int rc;
 	void *msg_head;
 	char * mydata;
+	int msg_size;
+	char msg[MSG_SIZE_MAX];
+	char *error_string = "ERROR";
+	char *queue_empty = "QUEUE EMPTY";
+	int rx_queue_cmd = 0;
+	int tx_queue_cmd = 0;
+	int queue_cmd = 0;
+	
+	msg_size = MSG_SIZE_MAX;
 	
 	if (info == NULL) {
 		goto out;
@@ -65,16 +128,47 @@ int doc_exmpl_echo(struct sk_buff *skb_2, struct genl_info *info) {
 		mydata = (char *)nla_data(na);
 		if (mydata == NULL) {
 			 printk("error while receiving data\n");
+			 goto out;
 		} else {
 			printk("received: %s\n", mydata);
 		}
 	} else {
 		printk("no info->attrs %i\n", DOC_EXMPL_A_MSG);
+		goto out;
+	}
+	
+	rx_queue_cmd = !strcmp("GETTS_RX",mydata);
+	tx_queue_cmd = !strcmp("GETTS_TX",mydata);
+	queue_cmd = rx_queue_cmd || tx_queue_cmd;
+	
+	if (queue_cmd) {
+			struct myqueue_element *qe = NULL;
+			
+			if(rx_queue_cmd) {
+				if (!is_queue_empty(&rx_queue)) {
+					qe = dequeue(&rx_queue);
+					sprintf_ts(msg,qe);
+				} else {
+					strcpy(msg,queue_empty);
+				}
+			}
+			else {
+				if (!is_queue_empty(&tx_queue)) {
+					qe = dequeue(&tx_queue);
+					sprintf_ts(msg,qe);
+				} else {
+					strcpy(msg,queue_empty);
+				}
+			}
+			
+			kfree(qe);
+	} else {
+		strcpy(msg,error_string);
 	}
 	
 	//Send a message back
-	//Allocate some memory, since the size is not yet known use NLMSG_GOODSIZE
-	skb = genlmsg_new(NLMSG_GOODSIZE, GFP_KERNEL);
+	//TRICK: If you dont know the size, use NLMSG_GOODSIZE
+	skb = genlmsg_new(msg_size, GFP_KERNEL);
 	if (skb == NULL) {
 		goto out;
 	}
@@ -89,14 +183,15 @@ int doc_exmpl_echo(struct sk_buff *skb_2, struct genl_info *info) {
 	 * u8 command index (why do we need this?)
 	 * */
 	 
-	msg_head = genlmsg_put(skb, 0, info->snd_seq+1, &doc_exmpl_gnl_family, 0, DOC_EXMPL_C_ECHO);
+	msg_head = genlmsg_put(skb, 0, info->snd_seq+1, 
+		&doc_exmpl_gnl_family, 0, DOC_EXMPL_C_GETTS);
 	if (msg_head == NULL) {
 		rc = -ENOMEM;
 		goto out;
 	}
 	
 	//Add a DOC_EXMPL_A_MSG attribute (actual value to be sent)
-	rc = nla_put_string(skb, DOC_EXMPL_A_MSG, "Hello World from kernel space");
+	rc = nla_put_string(skb, DOC_EXMPL_A_MSG, msg);
 	if (rc != 0) {
 		goto out;
 	}
@@ -117,17 +212,20 @@ out:
 
 //Commands: mapping between the command enumeration and the actual function
 struct genl_ops doc_exmpl_gnl_ops_echo = {
-	.cmd = DOC_EXMPL_C_ECHO,
+	.cmd = DOC_EXMPL_C_GETTS,
 	.flags = 0,
 	.policy = doc_exmpl_genl_policy,
-	.doit = doc_exmpl_echo,
+	.doit = doc_exmpl_getts,
 	.dumpit = NULL,
 };
 
 static int __init module_netlink_init(void) {
 	int rc;
 	struct genl_ops * ops = &doc_exmpl_gnl_ops_echo;
+	
+	getnstimeofday(&ts_current);
 	printk("Generic Netlink Example Module inserted.\n");
+	myprintk_ts(&ts_current);
 	
 	// Fill the family ops
 	doc_exmpl_gnl_family.ops = ops;
@@ -140,6 +238,12 @@ static int __init module_netlink_init(void) {
 	if (rc != 0) {
 		goto failure;
 	}
+	
+	queue_init(&rx_queue);
+	queue_init(&tx_queue);
+	
+	mod_timer(&mytimer, jiffies+TIMER_PERIOD);
+
 	return 0; 
 failure:
 	printk("An error occured while inserting the generic netlink example module\n");
@@ -148,13 +252,20 @@ failure:
 
 static void __exit module_netlink_exit(void) {
 	int ret;
+	
+	getnstimeofday(&ts_current);
 	printk("Generic Netlink Example Module unloaded.\n");
+	myprintk_ts(&ts_current);
+	del_timer(&mytimer);
 	
 	//Unregister the family
 	ret = genl_unregister_family(&doc_exmpl_gnl_family);
 	if(ret !=0) {
 		printk("Unregister family %i\n",ret);
 	}
+	
+	kfree_queue(&rx_queue);
+	kfree_queue(&tx_queue);
 }
 
 module_init(module_netlink_init);
--- userspace/userspace_netlink.c
+++ userspace/userspace_netlink.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <errno.h>
 #include <unistd.h>
 #include <poll.h>
@@ -24,7 +25,7 @@
 #define GENLMSG_PAYLOAD(glh) (NLMSG_PAYLOAD(glh, 0) - GENL_HDRLEN)
 #define NLA_DATA(na) ((void *)((char*)(na) + NLA_HDRLEN))
 
-#define MESSAGE_TO_KERNEL "Hello World!"
+#define NTIMES 100
 
 //Variables used for netlink
 
@@ -51,8 +52,45 @@ struct {
 	char buf[256];
 } nl_request_msg, nl_response_msg;
 
-int main(void) {
+struct myts {
+		uint64_t hour;
+		uint64_t min;
+		uint64_t sec;
+		uint64_t nsec;
+		uint64_t seq;
+		int valid;
+		int tx_rx;
+};
 
+void printf_myts(struct myts *ts)
+{
+	printf("============== TS ================ \n");
+	printf("Hour: %lu \n", ts->hour);
+	printf("Min: %lu \n", ts->min);
+	printf("Sec: %lu \n", ts->sec);
+	printf("Nsec: %lu \n", ts->nsec);
+	printf("Valid: %d \n", ts->valid);
+	printf("TX_RX: %s \n", (ts->tx_rx == 0) ? "Tx" : "Rx");
+	printf("Seq: %lu \n", ts->seq);
+	printf("================================== \n");
+}
+
+int main(int argc, char *argv[]) {
+
+	int ntimes;
+	int i;
+	char *buf;
+	char cmd[512];
+	struct myts ts;
+	
+	if (argc < 2) {
+		printf("Using %d times for the netlink test \n", NTIMES);
+		ntimes = NTIMES;
+	}
+	else {
+		ntimes = strtol(argv[1],(char **) NULL, 10);
+	}
+	
 	//Step 1: Open the socket. Note that protocol = NETLINK_GENERIC
 	nl_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_GENERIC);
 	if (nl_fd < 0) {
@@ -85,9 +123,9 @@ int main(void) {
 	//Populate the payload's "netlink attributes"
 	nl_na = (struct nlattr *) GENLMSG_DATA(&nl_request_msg);
 	nl_na->nla_type = CTRL_ATTR_FAMILY_NAME;
-	nl_na->nla_len = strlen("CONTROL_EXMPL") + 1 + NLA_HDRLEN;
+	nl_na->nla_len = strlen("HA_TS_FAMILY") + 1 + NLA_HDRLEN;
 	//Family name length can be upto 16 chars including \0
-	strcpy(NLA_DATA(nl_na), "CONTROL_EXMPL"); 
+	strcpy(NLA_DATA(nl_na), "HA_TS_FAMILY"); 
 	
 	nl_request_msg.n.nlmsg_len += NLMSG_ALIGN(nl_na->nla_len);
 	
@@ -131,66 +169,84 @@ int main(void) {
 	}
 	
 	//Step 4. Send own custom message
-	memset(&nl_request_msg, 0, sizeof(nl_request_msg));
-	memset(&nl_response_msg, 0, sizeof(nl_response_msg));
+	for(i = 0 ; i < ntimes ; i++) {
+		if (i % 2 == 0)
+			strcpy(cmd,"GETTS_TX");
+		else
+			strcpy(cmd,"GETTS_RX");
+			
+		memset(&nl_request_msg, 0, sizeof(nl_request_msg));
+		memset(&nl_response_msg, 0, sizeof(nl_response_msg));
 	
-	nl_request_msg.n.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
-	nl_request_msg.n.nlmsg_type = nl_family_id;
-	nl_request_msg.n.nlmsg_flags = NLM_F_REQUEST;
-	nl_request_msg.n.nlmsg_seq = 60;
-	nl_request_msg.n.nlmsg_pid = getpid();
-	nl_request_msg.g.cmd = 1; //corresponds to DOC_EXMPL_C_ECHO;
+		nl_request_msg.n.nlmsg_len = NLMSG_LENGTH(GENL_HDRLEN);
+		nl_request_msg.n.nlmsg_type = nl_family_id;
+		nl_request_msg.n.nlmsg_flags = NLM_F_REQUEST;
+		nl_request_msg.n.nlmsg_seq = 60;
+		nl_request_msg.n.nlmsg_pid = getpid();
+		nl_request_msg.g.cmd = 1; //corresponds to DOC_EXMPL_C_ECHO;
 	
-	nl_na = (struct nlattr *) GENLMSG_DATA(&nl_request_msg);
-	nl_na->nla_type = 1; // corresponds to DOC_EXMPL_A_MSG
-	nl_na->nla_len = sizeof(MESSAGE_TO_KERNEL)+NLA_HDRLEN; //Message length
-	memcpy(NLA_DATA(nl_na), MESSAGE_TO_KERNEL, sizeof(MESSAGE_TO_KERNEL));
-	nl_request_msg.n.nlmsg_len += NLMSG_ALIGN(nl_na->nla_len);
+		nl_na = (struct nlattr *) GENLMSG_DATA(&nl_request_msg);
+		nl_na->nla_type = 1; // corresponds to DOC_EXMPL_A_MSG
+		nl_na->nla_len = sizeof(cmd)+NLA_HDRLEN; //Message length
+		memcpy(NLA_DATA(nl_na), cmd, sizeof(cmd));
+		nl_request_msg.n.nlmsg_len += NLMSG_ALIGN(nl_na->nla_len);
 	
-	memset(&nl_address, 0, sizeof(nl_address));
-	nl_address.nl_family = AF_NETLINK;
+		memset(&nl_address, 0, sizeof(nl_address));
+		nl_address.nl_family = AF_NETLINK;
 	
-	//Send the custom message
-	nl_rxtx_length = sendto(nl_fd, (char *) &nl_request_msg, 
-		nl_request_msg.n.nlmsg_len, 0, 
-		(struct sockaddr *) &nl_address, sizeof(nl_address));
-	if (nl_rxtx_length != nl_request_msg.n.nlmsg_len) {
-		perror("sendto()");
-		close(nl_fd);
-		return -1;
+		//Send the custom message
+		nl_rxtx_length = sendto(nl_fd, (char *) &nl_request_msg, 
+			nl_request_msg.n.nlmsg_len, 0, 
+			(struct sockaddr *) &nl_address, sizeof(nl_address));
+		if (nl_rxtx_length != nl_request_msg.n.nlmsg_len) {
+			perror("sendto()");
+			close(nl_fd);
+			return -1;
+		}
+		printf("Sent to kernel: %s\n",cmd);
+	
+		//Receive reply from kernel
+		nl_rxtx_length = recv(nl_fd, &nl_response_msg, 
+			sizeof(nl_response_msg), 0);
+		if (nl_rxtx_length < 0) {
+			perror("recv()");
+			return -1;
+		}
+	
+		//Validate response message
+		if (nl_response_msg.n.nlmsg_type == NLMSG_ERROR) { //Error
+			printf("Error while receiving reply from kernel: NACK Received\n");
+			close(nl_fd);
+			return -1;
+		}
+	
+		if (nl_rxtx_length < 0) {
+			printf("Error while receiving reply from kernel\n");
+			close(nl_fd);
+			return -1;
+		}
+		if (!NLMSG_OK((&nl_response_msg.n), nl_rxtx_length)) {
+			printf("Error while receiving reply from kernel: Invalid Message\n");
+			close(nl_fd);
+			return -1;
+		}
+	
+		//Parse the reply message
+		nl_rxtx_length = GENLMSG_PAYLOAD(&nl_response_msg.n);
+		nl_na = (struct nlattr *) GENLMSG_DATA(&nl_response_msg);
+		printf("Kernel replied: %s\n",(char *)NLA_DATA(nl_na));
+		
+		buf = (char *)NLA_DATA(nl_na);
+		
+		if (strcmp(buf,"ERROR") && strcmp(buf,"QUEUE EMPTY")) {
+			sscanf(buf, "%lu:%lu:%lu:%lu:v%d:t%d:s%lu",
+				&ts.hour, &ts.min, &ts.sec, 
+				&ts.nsec, &ts.valid, 
+				&ts.tx_rx, &ts.seq);
+			printf_myts(&ts);
+		}
+		sleep(1);
 	}
-	printf("Sent to kernel: %s\n",MESSAGE_TO_KERNEL);
-	
-	//Receive reply from kernel
-	nl_rxtx_length = recv(nl_fd, &nl_response_msg, 
-		sizeof(nl_response_msg), 0);
-	if (nl_rxtx_length < 0) {
-		perror("recv()");
-		return -1;
-	}
-	
-	//Validate response message
-	if (nl_response_msg.n.nlmsg_type == NLMSG_ERROR) { //Error
-		printf("Error while receiving reply from kernel: NACK Received\n");
-		close(nl_fd);
-		return -1;
-	}
-	
-	if (nl_rxtx_length < 0) {
-		printf("Error while receiving reply from kernel\n");
-		close(nl_fd);
-		return -1;
-	}
-	if (!NLMSG_OK((&nl_response_msg.n), nl_rxtx_length)) {
-		printf("Error while receiving reply from kernel: Invalid Message\n");
-		close(nl_fd);
-		return -1;
-	}
-	
-	//Parse the reply message
-	nl_rxtx_length = GENLMSG_PAYLOAD(&nl_response_msg.n);
-	nl_na = (struct nlattr *) GENLMSG_DATA(&nl_response_msg);
-	printf("Kernel replied: %s\n",(char *)NLA_DATA(nl_na));
 	
 	//Step 5. Close the socket and quit
 	close(nl_fd);
